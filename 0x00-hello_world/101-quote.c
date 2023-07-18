83d3346e18e49b9d76b8bc29df8cad739f121cd0
Le-bo <lebohangmadikane@icloud.com>
2023-07-17 20:04:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.d>
+/**
+ * main - A program that prints a line to the standard erroe.
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
